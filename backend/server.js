5755d1a824a69a55930538f39c56a0334d00812d
Agustin Santicchia <santicchiaagus85@gmail.com>
2025-04-12 17:57:34 -0300

    Conectamos la base de datos Mongo

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json()); 
 
-app.get("/", (req, res) => {
+app.get("/products", (req, res) => {
     res.send("Hello World from Express.js!");
 });
 
